da248426bc0cc142ddfed4ce3530da050b13d6da
Paulo Sérgio Santos Junior <paulossjunior@gmail.com>
2023-11-06 18:33:54 -0300

    first commit

---
+++ intermediario/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Lista de tarefas como um exemplo de dados
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    task = request.form.get('task')
+    if task:
+        tasks.append(task)
+    return redirect(url_for('index'))
+
+@app.route('/complete_task/<int:task_id>')
+def complete_task(task_id):
+    if 0 <= task_id < len(tasks):
+        tasks.pop(task_id)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Exemplos simples de aplicações com Flask
+
+## Instalação 
+
+Digite o seguinte comando:
+'''
+pip install flask
+'''
+
+### Usar
+1. Basico: contem um código simples de Hello World
+2. Intermediario: contem um código de todo list
